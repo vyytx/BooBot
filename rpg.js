83a84590442a123f8a3b8eb4e8508b051ba04333
vyytx <dobeender@gmail.com>
2021-07-18 23:58:34 +0800

    update timetable

---
+++ rpg.js
@@ -0,0 +1,22 @@
+class Player {
+    constructor() {
+        this.HEL = 100; // health
+        this.DEF = 10;  // defense
+        this.SPD = 3;   // speed
+        this.STR = 10;  // strength
+        this.INT = 10;  // intelligence
+    }
+}
+
+class Item {
+    constructor(TYPE, DURABILITY) {
+        this.type = TYPE;
+        this.durability = DURABILITY;
+    }
+}
+
+class Tile {
+    constructor() {
+        
+    }
+}
